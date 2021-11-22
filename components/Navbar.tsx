f0173902bd6300437f9cfce6f8eb8b919b167c23
Jonathon Bloomfield <bloomfield.codes@gmail.com>
2021-11-22 14:15:22 -0500

    add animation styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,39 +1,46 @@
-import React from "react";
+import React, { useState} from "react";
 import Link from "next/link";
+import { animate, AnimateSharedLayout, motion } from "framer-motion";
+import { useRouter } from "next/router";
+import { projects } from "../data/projects";
 
-const projects: {id: number; name: string; href: string}[] = [
-    {
-        id: 1,
-        name: "Animer mon nom",
-        href: '/projet_1'
-    },
-    {
-        id: 2,
-        name:"Animer une histoire",
-        href: '/projet_2'
-    },
-    {
-        id: 3,
-        name:"Animer un jeu",
-        href: '/projet_3'
-    }
-]
 
 
 export default function Navbar(){
+  const [selected, setSelected] = useState(0);
+
+    const router = useRouter()
+    
+    const isActiveLink = function (href: string, currentPath: string): boolean {
+        if (href === '/'){
+            return false
+        }
+        return currentPath.startsWith(href)
+    }
+
     return (
+      <AnimateSharedLayout>  
         <nav>
           {
               projects.map(({href, name, id}) => (
-                  <h1 key = {name} className="title">
+                  <h1 key = {id} onClick={() => setSelected(id)}>
                     <Link  href = {href}>
-                      <a>
+                      <a className = {`title ${id === selected && "selected"}`}>
                         Projet {id}
+                        {isActiveLink(href, router.pathname) && (
+                          <motion.div layout
+                          transition = {{duration: 0.5}}
+                          className="underline"
+                          layoutId="underline"
+                          style={{ backgroundColor: '#ffc221' }}
+                        />
+                            )}
                       </a>
                     </Link>
                   </h1>
               ))
           }
         </nav>
+      </AnimateSharedLayout>
         )
 }
---
+++ data/projects.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export const projects: {id: number; name: string; href: string}[] = [
+    {
+        id: 1,
+        name: "Animer mon nom",
+        href: '/projet_1'
+    },
+    {
+        id: 2,
+        name:"Animer une histoire",
+        href: '/projet_2'
+    },
+    {
+        id: 3,
+        name:"Animer un jeu",
+        href: '/projet_3'
+    },
+    {
+        id: 4,
+        name:"Temps libre",
+        href: '/projet_4'
+    }
+]
